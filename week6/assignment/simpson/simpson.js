0d82646fe260f55e709ab86a137c628492304303
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-10-24 12:56:56 +0900

    week 9 exercise done

--- week6/assignment/simpson/simpson.js
+++ week6/assignment/simpson/simpson.js
@@ -9,7 +9,7 @@ window.onload = function init()
     renderer.setSize(canvas.width,canvas.height);
 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
+    scene.background = new THREE.Color(0xffffff);
 
     camera = new THREE.PerspectiveCamera(75,canvas.width / canvas.height,0.1, 1000);
     camera.rotation.y = 45/180*Math.PI;
@@ -19,29 +19,31 @@ window.onload = function init()
 
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    hlight = new THREE.AmbientLight (0x000000,50);
-    scene.add(hlight);
-    light = new THREE.PointLight(0xc4c4c4,10);
-    light.position.set(0,3000,5000);
+    // hlight = new THREE.AmbientLight (0x000000,50);
+    // scene.add(hlight);
+    let light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
     scene.add(light);
+    // light = new THREE.PointLight(0xc4c4c4,10);
+    // light.position.set(0,3000,5000);
+    // scene.add(light);
 
-    light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(5000,1000,0);
-    scene.add(light2);
-
-    light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,1000,-5000);
-    scene.add(light3);
-
-    light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-5000,3000,5000);
-    scene.add(light4);
+    // light2 = new THREE.PointLight(0xc4c4c4,10);
+    // light2.position.set(5000,1000,0);
+    // scene.add(light2);
+    //
+    // light3 = new THREE.PointLight(0xc4c4c4,10);
+    // light3.position.set(0,1000,-5000);
+    // scene.add(light3);
+    //
+    // light4 = new THREE.PointLight(0xc4c4c4,10);
+    // light4.position.set(-5000,3000,5000);
+    // scene.add(light4);
 
 
     const loader = new THREE.GLTFLoader();
     loader.load('./model/scene.gltf', function(gltf){
         simpson = gltf.scene.children[0];
-        simpson.scale.set(0.5,0.5,0.5);
+        simpson.scale.set(0.05,0.05,0.05);
         scene.add(gltf.scene);
         animate(1);
     }, undefined, function (error) {
